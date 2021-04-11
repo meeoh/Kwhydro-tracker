ec90f59468c221c846778c73f5547eddf70241c6
Shameel Abdullah <shameel.abdullah@gmail.com>
2021-04-11 19:51:26 -0400

    Change adapter to take in chat id as param

--- telegram_adapter.js
+++ telegram_adapter.js
@@ -1,14 +1,14 @@
 require("dotenv").config({ path: `${__dirname}/.env` });
-const { TELEGRAM_TOKEN: telegramToken, CHAT_ID: chatId } = process.env;
+const { TELEGRAM_TOKEN: telegramToken } = process.env;
 const required = ["TELEGRAM_TOKEN", "CHAT_ID"];
-if (!telegramToken || !chatId) {
+if (!telegramToken) {
   const missing = [];
   required.forEach((k) => (!process.env[k] ? missing.push(k) : null));
   console.log(`Missing ${missing.join(", ")}`);
   process.exit();
 }
 
-function sendMessage(msg) {
+function sendMessage(msg, chatId) {
   const TelegramBot = require("node-telegram-bot-api");
   const bot = new TelegramBot(telegramToken, { polling: false });
   bot.sendMessage(chatId, msg);
