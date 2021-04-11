47757e4fe6c7cc959ee3a526219edeea4474289f
Shameel Abdullah <shameel.abdullah@gmail.com>
2021-04-11 19:51:34 -0400

    Refactor to support multiple accounts

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 const puppeteer = require("puppeteer");
 
 const {
-  LOGIN: login,
-  PASSWORD: password,
+  LOGINS: loginsEnvVar,
+  PASSWORDS: passwordsEnvVar,
   CHROME_PATH: chromePath,
+  CHAT_IDS: chatIdsEnvVar,
   RASPBIAN,
 } = process.env;
 
-const required = ["LOGIN", "PASSWORD"];
-if (!login || !password) {
+const required = ["LOGINS", "PASSWORDS", "CHAT_IDS"];
+if (!loginsEnvVar || !passwordsEnvVar || !chatIdsEnvVar) {
   const missing = [];
   required.forEach((k) => (!process.env[k] ? missing.push(k) : null));
   console.log(`Missing ${missing.join(", ")}`);
@@ -25,50 +26,68 @@ function delay(time) {
 
 const messenger = require("./telegram_adapter");
 
+const logins = loginsEnvVar.split(",");
+const passwords = passwordsEnvVar.split(",");
+const chatIds = chatIdsEnvVar.split(",");
+
+if (logins.length !== passwords.length || logins.length !== chatIds.length) {
+  console.log(
+    "Logins, passwords, and chat ids env vars need to be arrays of same length"
+  );
+  process.exit();
+}
+
 (async () => {
-  const browser = RASPBIAN
-    ? await puppeteer.launch({ executablePath: "/usr/bin/chromium-browser" })
-    : await puppeteer.launch({});
-  const page = await browser.newPage();
+  for (var i = 0; i < logins.length; i++) {
+    const login = logins[i].trim();
+    const password = passwords[i].trim();
+    const chatId = chatIds[i].trim();
 
-  try {
-    await page.goto("https://www3.kwhydro.on.ca/app/login.jsp");
-    await page.waitForSelector("#accessCode").then(async () => {
-      await page.focus("#accessCode");
-      await page.keyboard.type(login);
-    });
-    await page.waitForSelector("#password").then(async () => {
-      await page.focus("#password");
-      await page.keyboard.type(password);
-      await page.keyboard.press("Enter");
-    });
-    await delay(3000);
+    const browser = RASPBIAN
+      ? await puppeteer.launch({ executablePath: "/usr/bin/chromium-browser" })
+      : await puppeteer.launch({});
+    const page = await browser.newPage();
 
-    await page.goto(
-      "https://www3.kwhydro.on.ca/app/capricorn?para=smartMeterConsumInqV2&inquiryType=hydro&tab=SMCONSUM"
-    );
+    try {
+      await page.goto("https://www3.kwhydro.on.ca/app/login.jsp");
+      await page.waitForSelector("#accessCode").then(async () => {
+        await page.focus("#accessCode");
+        await page.keyboard.type(login);
+      });
+      await page.waitForSelector("#password").then(async () => {
+        await page.focus("#password");
+        await page.keyboard.type(password);
+        await page.keyboard.press("Enter");
+      });
+      await delay(3000);
 
-    await delay(3000);
+      await page.goto(
+        "https://www3.kwhydro.on.ca/app/capricorn?para=smartMeterConsumInqV2&inquiryType=hydro&tab=SMCONSUM"
+      );
 
-    await page.hover(
-      ".highcharts-series-group > .highcharts-series-0 > rect:nth-last-child(2)"
-    );
+      await delay(3000);
 
-    const text = [];
-    await page.waitForSelector(".highcharts-tooltip tspan").then(async () => {
-      elements = await page.$$(".highcharts-tooltip tspan");
-      for (var i = 0; i < elements.length; i++) {
-        text.push(
-          await page.evaluate((element) => element.textContent, elements[i])
-        );
-      }
-    });
+      await page.hover(
+        ".highcharts-series-group > .highcharts-series-0 > rect:nth-last-child(2)"
+      );
 
-    messenger.sendMessage(
-      `Electricity usage on ${text[0]} cost ${text[2]} (${text[1]})`
-    );
-  } catch (e) {
-  } finally {
-    await browser.close();
+      const text = [];
+      await page.waitForSelector(".highcharts-tooltip tspan").then(async () => {
+        elements = await page.$$(".highcharts-tooltip tspan");
+        for (var i = 0; i < elements.length; i++) {
+          text.push(
+            await page.evaluate((element) => element.textContent, elements[i])
+          );
+        }
+      });
+
+      messenger.sendMessage(
+        `Electricity usage on ${text[0]} cost ${text[2]} (${text[1]})`,
+        chatId
+      );
+    } catch (e) {
+    } finally {
+      await browser.close();
+    }
   }
 })();
